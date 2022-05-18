77a9eadccbbef34c45fd603f1ca10a7f83317779
luca-rotondaro <98099333+luca-rotondaro@users.noreply.github.com>
2022-05-18 08:19:50 +0200

    TPC-4 test

--- 02_WebApp/main.tf
+++ 02_WebApp/main.tf
@@ -3,6 +3,11 @@ resource "azurerm_resource_group" "example" {
   location = var.resource_group_location
 }
 
+resource "azurerm_resource_group" "example2" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
 resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.example.location
